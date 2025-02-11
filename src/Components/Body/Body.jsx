0c0db4b40048bd66da3cfd38d38be3666d81a2d3
raunit1610 <raunitraj2018@gmail.com>
2025-02-11 20:53:31 +0530

    modify backgroud of expense form and header

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,39 +1,40 @@
 import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import ExpenseList from "./ExpenseList";
-import './Body.css';
+import "./Body.css";
 
 const Body = () => {
   const [list, setList] = React.useState([]);
 
   React.useEffect(() => {
-    const savedExpenses = localStorage.getItem('expenses');
+    const savedExpenses = localStorage.getItem("expenses");
     if (savedExpenses) {
       setList(JSON.parse(savedExpenses));
     }
   }, []);
 
-
   function addItem(item) {
-    setList(prevList => {
+    setList((prevList) => {
       const newList = [...prevList, item];
-      localStorage.setItem('expenses', JSON.stringify(newList));
+      localStorage.setItem("expenses", JSON.stringify(newList));
       return newList;
     });
   }
 
   function removeItem(id) {
-    setList(prevList => {
-      const newList = prevList.filter(item => item.id !== id);
-      localStorage.setItem('expenses', JSON.stringify(newList));
+    setList((prevList) => {
+      const newList = prevList.filter((item) => item.id !== id);
+      localStorage.setItem("expenses", JSON.stringify(newList));
       return newList;
     });
   }
 
   return (
     <div className="BodyList">
-      <ExpenseForm addItem={addItem} data={list}/>
-      <ExpenseList itemList={list} removeItem={removeItem} />
+      <React.Fragment>
+        <ExpenseForm addItem={addItem} data={list} />
+        <ExpenseList itemList={list} removeItem={removeItem} />
+      </React.Fragment>
     </div>
   );
 };
